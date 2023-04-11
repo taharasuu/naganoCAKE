30dce2f44fd3931f3f473f9685458aa88f260e5d
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2023-04-11 05:00:54 +0000

    naganocake

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,7 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.page(params[:page]).per(8)
+    @item = Item.all
   end
 
   def show
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -42,7 +42,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :ilast_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
     end
 
     def after_sign_in_path_for(resource)
